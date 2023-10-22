9ae60f625e69169f7c61a4425f47581209ffc600
Jake Kravas <jakek237@arizona.edu>
2023-10-22 15:13:32 -0700

    finished project

---
+++ public_html/script.js
@@ -0,0 +1,63 @@
+/* Jake Kravas
+This file sends user-inputted chat data,
+such as their alias and message to server.js.
+It also checks for new chats every 1 second
+*/
+
+let sendBtn = document.getElementById('send-btn');
+let messageForm = document.getElementById('message-form');
+
+// Create and save message
+messageForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  let aliasVal = document.getElementById('alias-input').value;
+  let messageVal = document.getElementById('message-input').value;
+
+  if (aliasVal && messageVal) {
+    let currentTime = new Date().getTime();
+    let newMessageHTML = `<p class='chat'><strong>${aliasVal}: </strong>${messageVal}</p>`;
+    let chatContent = document.getElementById('chat-content');
+
+    const info = {
+      time: currentTime,
+      alias: aliasVal,
+      message: messageVal
+    }
+
+    let post = fetch('http://localhost:3000/createchat', {
+      method: 'POST',
+      body: JSON.stringify(info),
+      headers: { 'Content-Type': 'application/json'}
+    });
+
+    post.then((response) => {
+      return response.text();
+    }).then((text) => {
+      console.log(text);
+      chatContent.innerHTML = chatContent.innerHTML + newMessageHTML;
+    });
+
+  }
+
+});
+
+window.onload = loadAllMessages();
+
+function loadAllMessages() {
+
+  let url = 'http://localhost:3000/get';
+
+  fetch(url).then((response) => {
+    return response.text();
+  }).then((text) =>{
+
+    // insert messages into HTML
+    document.getElementById('chat-content').innerHTML = text;
+  }).catch( (error) => {
+    console.log(error);
+  });
+}
+
+// look for new messages every 1 second
+window.setInterval(loadAllMessages, 1000);
---
+++ server.js
@@ -0,0 +1,76 @@
+/* Jake Kravas
+This file receives data from the front end,
+and adds chat aliases and messages to the database
+*/
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const bp = require('body-parser');
+const mongoose = require('mongoose');
+
+// Database stuff
+// const MONGODB_URL = 'mongodb://localhost:27017';
+const MONGODB_URL = 'mongodb://localhost:27017/chatty';
+
+let Schema = mongoose.Schema;
+
+let ChatSchema = new Schema({
+  time: Number,
+  alias: String,
+  message: String
+});
+
+let Chat = mongoose.model('chat', ChatSchema );
+
+const db = async() => {
+  try {
+    const con = await mongoose.connect(MONGODB_URL);
+    console.log('Database Connected');
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+db()
+
+app.use(express.static('/assets'), express.static(__dirname + '/public_html'));
+app.use(bp.json());
+
+app.listen(port, () =>
+  console.log(`App listening at http://localhost:${port}`)
+);
+
+app.get('/get', async function(req, res) {
+  let chats = await Chat.find({});
+  let chatsHTML = '';
+  
+  // append data from each chat to HTML-compatible string
+  for (let i = 0; i < chats.length; i++) {
+    chatsHTML += `<p class='chat'><strong>${chats[i].alias}: </strong>${chats[i].message}</p>`
+  }
+  
+  res.end(chatsHTML);
+})
+
+
+app.post('/createchat', async function(req, res) {
+
+  let time = req.body.time;
+  let message = req.body.message;
+  let alias = req.body.alias;
+
+  let newChat = new Chat({
+    time: time,
+    alias: alias,
+    message: message,
+  });
+
+  try {
+    await newChat.save();
+    return res.send('Message saved successfully');
+  } catch (err) {
+    return res.status(500).send('Failed to save message');
+  }
+
+})
