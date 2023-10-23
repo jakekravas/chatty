adf8116c539024f71a90e690a849e1c0c655e380
Jake Kravas <jakek237@arizona.edu>
2023-10-23 15:20:03 -0700

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ and adds chat aliases and messages to the database
 
 const express = require('express');
 const app = express();
-const port = 80;
+const port = 8080;
 const bp = require('body-parser');
 const mongoose = require('mongoose');
 
